dc043324a7142e2d62a962d0f4b983543d176be0
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-05-21 15:21:52 +0100

    Updated

--- client/index.ts
+++ client/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
-  uri: '/api/graphql',
+  uri: process.env.PORT || 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
   credentials: 'same-origin'
 });
