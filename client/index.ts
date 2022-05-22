c9d289a38ef35408c3bbe8eb797bfd6eff037cf5
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-05-22 13:12:52 +0100

    Updated

--- client/index.ts
+++ client/index.ts
@@ -1,7 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const httpLink = new HttpLink({
+  uri: '/api/graphql'
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.PORT || 'http://localhost:3000/api/graphql',
-  cache: new InMemoryCache(),
-  credentials: 'same-origin'
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache()
 });
